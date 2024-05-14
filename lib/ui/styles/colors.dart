634a721201fb48c179436ac51cc82c60e62a6a1a
Phuoc Nguyen <phuocnguyen@phuocs-mbp.0.31.168.192.in-addr.arpa>
2024-05-14 17:38:12 +0700

    color

--- lib/ui/styles/colors.dart
+++ lib/ui/styles/colors.dart
@@ -4,14 +4,14 @@ import 'package:flutter/material.dart';
 const klBackgroundColor = Color(0xffffffff);
 const klCanvasColor = Color(0xcc000000);
 const klPageBackgroundColor = Color(0xfff3f7fa);
-const klPrimaryColor = Color(0xffef5388);
+const klPrimaryColor = Color(0xff006783);
 const klPrimaryTextColor = Color(0xff45536d);
 
 /// Dark Theme
 const kdBackgroundColor = Color(0xff294261);
 const kdCanvasColor = Color(0xccffffff);
 const kdPageBackgroundColor = Color(0xff233354);
-const kdPrimaryColor = Color(0xfff279a2);
+const kdPrimaryColor = Color(0xffA93A6F);
 const kdPrimaryTextColor = Color(0xfffefefe);
 
 /// Common
